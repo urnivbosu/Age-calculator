615bef1a4e3d2f4fa9023333cefc583a8b013697
URNIV BOSU <urnivbosu.vitc@gmail.com>
2023-10-23 15:54:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import Agecalcform from "./Agecalc";
+import Ageresult from "./Ageresult";
 import './App.css';
+import {
+  differenceInDays,
+  differenceInMonths,
+  differenceInYears,
+} from 'date-fns';
 
 function App() {
+  const [age,setage]=useState(null);
+
+const calculateage=(birthdate)=>{
+  const today= new Date();
+  const birthdateobj= new Date(birthdate);
+  const ageyears= differenceInYears(today,birthdateobj);
+  const agemonths= differenceInMonths(today,birthdateobj)%12;
+  const agedays= differenceInDays(today,new Date(today.getFullYear(), today.getMonth(), birthdateobj.getDate()));
+
+  setage({
+      years: ageyears,
+      months: agemonths,
+      days: agedays,
+    });
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Age Calculator</h1>
+      <p>Write Your date of birth</p>
+      <Agecalcform calculateage={calculateage}/>
+      {age && <Ageresult age={age} />}
     </div>
   );
 }
